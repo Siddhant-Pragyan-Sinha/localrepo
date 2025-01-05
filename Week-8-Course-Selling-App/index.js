4179c4300eed42146d58d2e09bd365a2e0000e77
Siddhant-Pragyan-Sinha <siddhantpragyansinha16@gmail.com>
2025-01-05 20:27:20 +0530

    week-8 course selling app complete

--- Week-8-Course-Selling-App/index.js
+++ Week-8-Course-Selling-App/index.js
@@ -1,37 +1,20 @@
 const express = require("express");
+const mongoose = require("mongoose");
 
+const { userRouter } = require("./routes/user");
+const { courseRouter } = require("./routes/course");
+const { adminRouter } = require("./routes/admin");
 const app = express();
 
-app.post("/user/signup", function(req, res) {
-    res.json({
-        message: "signup endpoint"
-    })
-})
-
-app.post("/user/signin", function(req, res) {
-    res.json({
-        message: "signup endpoint"
-    })
-})
-
-app.get("/user/purchases", function(req, res) {
-    res.json({
-        message: "signup endpoint"
-    })
-})
-
-app.post("/course/purchase", function(req, res) {
-    // you would expect the user to pay you money
-    res.json({
-        message: "signup endpoint"
-    })
-})
-
-app.get("/courses", function(req, res) {
-    res.json({
-        message: "signup endpoint"
-    })
-})
+app.use("/api/v1/user", userRouter);
+app.use("/api/v1/admin", adminRouter);
+app.use("/api/v1/course", courseRouter);
 
 
-app.listen(3000);
+async function main() {
+    await mongoose.connect("mongodb+srv://Siddhant-Pragyan-Sinha:Harsh280204@cluster0.8gjzp.mongodb.net/coursera-app")
+    app.listen(3000);
+    console.log("listening on port 3000")
+}
+
+main()
