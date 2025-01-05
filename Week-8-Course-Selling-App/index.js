9a8583b58ab4a2c77d44e80143f59790f03ddffe
Siddhant-Pragyan-Sinha <siddhantpragyansinha16@gmail.com>
2025-01-06 03:35:35 +0530

    Course selling app backend completed

--- Week-8-Course-Selling-App/index.js
+++ Week-8-Course-Selling-App/index.js
@@ -1,3 +1,5 @@
+require('dotenv').config()
+console.log(process.env.MONGO_URL)
 const express = require("express");
 const mongoose = require("mongoose");
 
@@ -5,14 +7,15 @@ const { userRouter } = require("./routes/user");
 const { courseRouter } = require("./routes/course");
 const { adminRouter } = require("./routes/admin");
 const app = express();
+app.use(express.json());
+
 
 app.use("/api/v1/user", userRouter);
 app.use("/api/v1/admin", adminRouter);
 app.use("/api/v1/course", courseRouter);
 
-
 async function main() {
-    await mongoose.connect("mongodb+srv://Siddhant-Pragyan-Sinha:Harsh280204@cluster0.8gjzp.mongodb.net/coursera-app")
+    await mongoose.connect(process.env.MONGO_URL)
     app.listen(3000);
     console.log("listening on port 3000")
 }
