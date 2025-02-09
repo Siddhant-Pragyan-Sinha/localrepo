4f2a0144a9a4c5928d2ad7b510775bf73957343e
Siddhant-Pragyan-Sinha <siddhantpragyansinha16@gmail.com>
2025-02-09 15:34:25 +0530

    Completed Prisma and started basic Next.js learning

---
+++ Week-18-Prisma-and-ORMs/week-18.2-prisma-advanced-introducting-ssrs/app/(auth)/signin.tsx
@@ -0,0 +1,5 @@
+import { Signin as SigninComponent } from "@/app/components/signin/page";
+
+export default function Signin() {
+    return <SigninComponent />
+}
---
+++ Week-18-Prisma-and-ORMs/week-18.2-prisma-advanced-introducting-ssrs/app/(auth)/signup.tsx
@@ -0,0 +1,5 @@
+import { Signup as SignupComponent } from "@/app/components/signup/page";
+
+export default function Signup() {
+    return <SignupComponent />
+}
